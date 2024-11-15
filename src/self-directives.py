ff83975e404a3110e3571458a696fa9874474b94
Johann Diedrick <johann@somewheregood.com>
2024-11-15 16:51:36 -0500

    init commit

---
+++ src/self-directives.py
@@ -0,0 +1,8 @@
+def get_first_directive():
+    return "Work at the edge of your abilities."
+
+def get_second_directive():
+    return "Build your volitional muscles."
+
+def get_third_directive():
+    return "Learn generously."
